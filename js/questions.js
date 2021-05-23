61b8fbac1a93b954b9e987257878efe6c36cd775
Bhavesh <bhavesharora127@gmail.com>
2021-05-23 21:00:08 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,116 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+  {
+    numb: 6,
+    question: "Choose the correct HTML tag for largest heading",
+    answer: "h1",
+    options: [
+      "heading",
+      "h6",
+      "head",
+      "h1"
+    ]
+  },
+  {
+    numb: 7,
+    question:"What is the correct HTML tag for inserting a line break? ",
+    answer: "br",
+    options: [
+      "br",
+      "lb",
+      "break",
+      "h1"
+    ]
+  },
+  {
+    numb: 8,
+    question: "How can you make a numbered list?",
+    answer: "ol",
+    options: [
+      "ol",
+      "h1",
+      "dl",
+      "list"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which property is used to change the bg color?",
+    answer: "background-color",
+    options: [
+      "color",
+      "background-color",
+      "bgcolor",
+      "h1"
+    ]
+  },
+];
